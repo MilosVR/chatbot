5df8667585a84dd61a5dcfda0b879dc37924b35e
MilosVR <mecazavod@gmail.com>
2022-01-10 14:38:51 +0100

    test

--- src/Dictaphone/DIctaphone.jsx
+++ src/Dictaphone/DIctaphone.jsx
@@ -1,30 +1,39 @@
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useEffect, useState } from 'react';
 
 const Dictaphone = () => {
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
+    const [response, setResponse] = useState('')
+    useEffect(() => {
+        if (transcript.includes('glavni grad Španije')) {
+          setResponse("Glavni grad Spanije je Madrid.")
+        }
+    }, [transcript])
 
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
-  }
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
 
-  SpeechRecognition.startListening({
-    continuous: true,
-    language: 'sr-SP'
-  })
+    if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+    }
 
-  return (
-    <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
-    </div>
-  );
+    SpeechRecognition.startListening({
+        continuous: true,
+        language: 'sr-SP'
+    })
+
+    return (
+        <div>
+        <p>Microphone: {listening ? 'on' : 'off'}</p>
+        <button onClick={SpeechRecognition.startListening}>Start</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button onClick={resetTranscript}>Reset</button>
+        <p>{transcript}</p>
+        <p>{response}</p>
+        </div>
+    );
 };
 export default Dictaphone;
