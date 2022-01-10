840d1fffafa8cd633f1a5c06cc2d265e3f7f1ba4
MilosVR <mecazavod@gmail.com>
2022-01-10 14:19:44 +0100

    first init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './css/main.css'
+import Dictaphone from './Dictaphone/DIctaphone';
+
+function App() {
+    return ( 
+        <div>
+            <h1>Test</h1>
+            <Dictaphone />
+        </div>
+    );
+}
+
+export default App;
